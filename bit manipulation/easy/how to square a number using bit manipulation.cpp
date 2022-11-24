d05ea115323519493011e9ff1ba8887889a0d984
Sunny Kumar Pal <111234635+Sunnykumarpal1@users.noreply.github.com>
2022-11-25 01:13:37 +0530

    Update how to square a number using bit manipulation.cpp

--- bit manipulation/easy/how to square a number using bit manipulation.cpp
+++ bit manipulation/easy/how to square a number using bit manipulation.cpp
@@ -1,3 +1,5 @@
+
+//  1 st approach 
 #include<bits/stdc++.h>
 using namespace std;
 int Square(int x){
@@ -9,8 +11,10 @@ int Square(int x){
     //  and squre of odd is (2x+1)^2=4x^2+4x+1
     //  even square is 4x
     if(x&1){
+//         res=4*Square(x>>1)+4*Squate(x>>1)+1
        res= 4*Square(x/2)+4*Square(x/2)+1;
     }else{
+//         res=4*Square(x>>1)
         res=4*Square(x/2);
     }
     return res;
@@ -20,3 +24,17 @@ int main(){
     // here we need to find square of a number
    cout<< Square(val);
 }
+//  2nd approach ->thsi approch is also done at O(logn)
+algo -> here we will checking if the bit is 1 then we will find the power of the bit 
+int Square(int x){
+   int power=0,temp=x;
+   int res=0;
+    while(temp){
+     if(temp&1){
+       res+=(x<<power);
+       }
+       power++;
+       temp=temp>>1;
+       }
+       cout<<res;
+    }
