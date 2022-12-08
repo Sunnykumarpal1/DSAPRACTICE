092b992a7c15f98eee8c777eed4e62a4ad5728c6
Sunny Kumar Pal <111234635+Sunnykumarpal1@users.noreply.github.com>
2022-12-09 02:12:16 +0530

    Create merge k sorted list.cpp

---
+++ heap/MEDIUM/merge k sorted list.cpp
@@ -0,0 +1,31 @@
+using prority queue
+class comp{
+    public: bool operator()(ListNode* a,ListNode* b){
+         return a->val>b->val;
+     }
+ };
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*,vector<ListNode*>,comp>pq;
+        for(auto i:lists){
+            if(i){
+                pq.push(i);
+            }
+        }
+        ListNode* dummy=new ListNode(0);
+        ListNode* ptr=dummy;
+        while(!pq.empty()){
+          ListNode* t=pq.top();
+          pq.pop();
+          ptr->next=t;
+          ptr=ptr->next;
+          ptr->next=NULL;
+          if(t->next!=NULL){
+              t=t->next;
+              pq.push(t);
+          }
+        }
+       return  dummy->next;
+    }
+};
